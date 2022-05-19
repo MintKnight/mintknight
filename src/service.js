11461644c1159075e5af07717c2cb1fcf022d8c1
Dani Gilabert <dani@giladev.com>
2022-05-19 17:06:02 +0200

    Fixed wait task

--- src/service.js
+++ src/service.js
@@ -17,7 +17,7 @@ class MintKnight extends MintKnightBase {
    * @param {string} taskId task ID
    * @param {integer} times It will try every 5 seconds for n times.
    */
-  waitTask(taskId, times = 20) {
+  waitTask(taskId, times = 100) {
     if (!taskId) return { addressTo: false };
     return new Promise(async (resolve) => {
       this.mkLog('waiting for Task to end...');
@@ -36,6 +36,8 @@ class MintKnight extends MintKnightBase {
           return resolve(data);
         }
       }
+      this.mkLog('Task still pending');
+      return resolve({ state: 'failed' });
     });
   }
 
